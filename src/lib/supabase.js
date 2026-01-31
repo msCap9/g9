4ed6b01f1f11ba2ee356c2e43e894227e9cac1b0
msCap9 <canehyri@gmail.com>
2026-01-31 16:20:10 +0300

    Fix Supabase API key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://kutozxmqorultccxofkw.supabase.co';
-const supabaseKey = 'sb_publishable_CiKP-gOZzSV1nSX-X9_NLA_JBx7Wxq8';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1dG96eG1xb3J1bHRjY3hvZmt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MTUwMDAsImV4cCI6MjA4NTM5MTAwMH0.U3-1XsRro0OzSq-5phZb71GEPj5GEULxdbnAQMDqXEM';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
